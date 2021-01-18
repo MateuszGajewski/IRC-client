1e1bab9af27ded96b4c849d061fab944aa466b30
MateuszGajewski <mg96272@gmail.com>
2021-01-18 20:48:06 +0100

    Update irc_client.py

--- irc_client.py
+++ irc_client.py
@@ -1,42 +1,41 @@
 import socket
 
-class IRCClient:
+#Klasa implementująca komunikacje sieciową
 
-    def __init__(self, server='192.168.1.5', port=1234):
+class IRCClient:
+    #Inicjalizajca gniazda sieciowego
+    def __init__(self, server="127.0.0.1", port=1234):
         self.server = server
         self.port = port
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+        #self.connection.settimeout(0.00001)
+    #Połaczenie ze wskazanym serwerem przez wskazany port
     def connect(self):
         self.connection.connect((self.server, self.port))
-        """
-        self.connection.bind((self.server, self.port))
-        self.connection.listen(2)
-        self.conection, addr = self.connection.accept()"""
 
+    #Odbieranie wiadomości
     def get_response(self):
         a = ''
-        b = self.connection.recv(1).decode('utf-8')
-        a = b
-        while(b!='\n'):
-            a +=b
+        try:
             b = self.connection.recv(1).decode('utf-8')
+            a = b
+            while(b!='\n'):
+                a +=b
+                b = self.connection.recv(1).decode('utf-8')
 
-        return a
 
-    def send_cmd(self, cmd, message):
-        command = "{} {}".format(cmd, message).encode("utf-8")
-        self.connection.send(command)
+            return a
+        except:
+            pass
 
-    def join_channel(self, channel):
-        cmd = "JOIN"
-        self.send("{} {}".format(cmd, channel))
 
-    def send_message(self, message):
-        cmd = "MSSG"
-        self.send("{} {}".format(cmd, message))
-
+    #Wysyłanie wiadomości
     def send(self, message):
-        print(message)
-        self.connection.send(message.encode('utf-8'))
+        try:
+            i = 0
+            while(message[i]!= '\n'):
+                self.connection.send(message[i].encode('utf-8'))
+                i+=1
+            self.connection.send(message[i].encode('utf-8'))
+        except:
+            pass
