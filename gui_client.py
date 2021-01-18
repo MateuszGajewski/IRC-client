2362033391b2a0367a440b13fe2fdfe24185931b
MateuszGajewski <mg96272@gmail.com>
2021-01-18 20:47:44 +0100

    Update gui_client.py

--- gui_client.py
+++ gui_client.py
@@ -1,12 +1,15 @@
 from irc_client import IRCClient
-import code
 import threading
 import time
 from tkinter import *
 from tkinter import messagebox, ttk
 from tkinter.scrolledtext import ScrolledText
+from datetime import datetime
+
+#Klasa implementująca interfejs graficzny
 
 class Window(Frame):
+    #Inicjalizacja okna programu
     def __init__(self, master):
         Frame.__init__(self, master)
         self.root = master
@@ -14,18 +17,19 @@ class Window(Frame):
         self.grid()
         self.tabs = {}
         self.run_login_window()
-        self.channels = [
-                            'No channel',
-                         ]
+        self.channels = ['No channel']
         self.start_channel = 'No channel'
         self.current_tab_name = self.start_channel
 
+    #Wywoływanie okna logowania
+    def run_login_window(self):
 
-    def run_login_window(self):  # Builds the UI for the username entry window
+        root.title('~~  IRC  ~~')
+
         self.loginWindow = Toplevel()
         self.loginWindow.transient(root)
         w = 280
-        h = 240
+        h = 125
         sw = self.loginWindow.winfo_screenwidth()
         sh = self.loginWindow.winfo_screenheight()
         x = (sw - w) / 2
@@ -33,61 +37,54 @@ class Window(Frame):
         self.loginWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
         self.server_ip_input = Entry(self.loginWindow, width=24)
-        self.server_ip_input.place(x=75, y=10)
+        self.server_ip_input.place(x=125, y=10)
         self.server_ip_input.insert(0, '127.0.0.1')
-        Label(self.loginWindow, text='Server IP').place(x=5, y=10)
+        Label(self.loginWindow, text='Adres IP serwera').place(x=5, y=10)
 
         self.server_port_input = Entry(self.loginWindow, width=24)
-        self.server_port_input.place(x=75, y=32)
-        self.server_port_input.insert(0, '3000')
-        Label(self.loginWindow, text='Server Port').place(x=5, y=32)
+        self.server_port_input.place(x=125, y=35)
+        self.server_port_input.insert(0, '1234')
+        Label(self.loginWindow, text='Port serwera').place(x=5, y=35)
 
         self.username_input = Entry(self.loginWindow, width=24)
-        self.username_input.place(x=75, y=54)
+        self.username_input.place(x=125, y=60)
         self.username_input.focus_force()
-        Label(self.loginWindow, text='Username').place(x=5, y=54)
+        Label(self.loginWindow, text='NICK').place(x=5, y=60)
 
-        self.usernameButton = Button(self.loginWindow, text='Connect', command=self.connect_to_server, height=2, width=8)
+        self.usernameButton = Button(self.loginWindow, text='POŁĄCZ', command=self.connect_to_server, height=1, width=8)
         self.usernameButton.bind('<Return>', self.connect_to_server)
-        self.usernameButton.place(x=90, y=185)
+        self.usernameButton.place(x=100, y=90)
 
+    #Funkcja służąca do nawiązywania połączenia z serwerem
     def connect_to_server(self, event=None):
-
         username = self.username_input.get()
+        #Ustalanie początkoweg nicku
         if username == '':
-            messagebox.showinfo(message='You must enter a username', icon='warning')
+            messagebox.showinfo(message='Podaj NICK.', icon='warning')
         elif ' ' in username:
-            messagebox.showinfo(message='Username cannot contain spaces', icon='warning')
-        self.nick = username
-        self.client.server = self.server_ip_input.get()
-        self.client.port = int(self.server_port_input.get())
-        try:
-            self.client.connect()
-        except (ConnectionRefusedError):
-            messagebox.showinfo(message='Cannot connect to the server', icon='warning')
-            return
-        except (OSError):
-            print('already connected')
-            pass
-        self.client.send("NICK " + username+"\n")
-        time.sleep(1)
+            messagebox.showinfo(message='Nazwa użytkownika nie może zawierać spacji.', icon='warning')
+        else:
+            self.nick = username
+            self.client.server = self.server_ip_input.get()
+            self.client.port = int(self.server_port_input.get())
+            try:
+                self.client.connect()
+            except (ConnectionRefusedError) as e:
+                messagebox.showinfo(message='Nie można połączyć się z serwerem.', icon='warning')
+                return
+            except (OSError) as e:
+                messagebox.showinfo(message=e, icon='warning')
+                return
+            self.client.send("NICK " + username+"\n")
+            time.sleep(1)
 
-        """response = self.client.get_response()
+            self.run_chat_window()
+            self.start_reading()
+            self.loginWindow.destroy()
+    #Wywoływanie okna czatu
+    def run_chat_window(self):
 
-        print(response)
-        if response[0:8] != 'signedin':
-            messagebox.showinfo(message='Some error occured: ' + response, icon='warning')
-            return
-
-        self.client.send('ok')"""
-
-        self.run_chat_window()
-        self.start_reading()
-        self.loginWindow.destroy()
-
-    def run_chat_window(self):  # Builds the UI for the main window
-
-        root.title('IRC')
+        root.title('~~  IRC  ~~')
 
         self.tab_control = ttk.Notebook(root)
         self.tab_control.grid(row=0, column=0, sticky=W+E+N+S)
@@ -102,13 +99,13 @@ class Window(Frame):
             self.current_tab_name = channel
         self.tab_control.bind('<Button-1>', self.switch_tab)
 
+    #Zmiana nazwy kanału przy dołączaniu do nowego kanału
     def switch_tab(self, event):
         clicked_tab = self.tab_control.tk.call(self.tab_control._w, "identify", "tab", event.x, event.y)
         channel_name = self.tab_control.tab(clicked_tab, "text")
 
         if channel_name != self.current_tab_name:
 
-            #clear old chat box
             old_text_box = self.tabs[self.current_tab_name]['textbox']
             old_text_box.delete("1.0", END)
 
@@ -116,7 +113,7 @@ class Window(Frame):
             channel_number = channel_name[-1]
             self.join_channel(channel_number)
 
-
+    #Ustalanie parametrów okna czatu
     def create_channel_tab(self, tab_name):
         self.textboxframe = ttk.Frame(self.tab_control)
         self.textboxframe.grid(row=0, column=0, sticky=N + S + E + W)
@@ -129,7 +126,7 @@ class Window(Frame):
         self.entry_box.grid(row=2, column=0, padx=(10, 0), pady=(0, 10), sticky=N + S + E + W)
         self.entry_box.bind('<Return>', lambda: self.write(tab_name))
 
-        self.sendButton = Button(self.textboxframe, text='send', command=lambda: self.write(tab_name), height=2, width=8)
+        self.sendButton = Button(self.textboxframe, text='WYŚLIJ', command=lambda: self.write(tab_name), height=2, width=8)
         self.sendButton.bind('<Return>', lambda: self.write(tab_name))
         self.sendButton.grid(row=4, column=0, padx=(10, 0), pady=(0, 10), sticky=N + S + E + W)
 
@@ -143,47 +140,73 @@ class Window(Frame):
 
         self.tab_control.add(self.textboxframe, text=tab_name)
 
+    #Inicjalizacja wątku odbierającego wiadomości
     def start_reading(self):
         thread = threading.Thread(target=self.read, daemon=True)
         thread.start()
 
+    #Pobieranie tekstu z formularza
     def get_text(self, scrolled_text):
         text = scrolled_text.get("1.0", 'end-1c')
         scrolled_text.delete('1.0', END)
         scrolled_text.focus_force()
         return text
 
+    #Wysyłanie wiadomości przez użytkownika
     def write(self, tab_name):
         to_send =''
         entry_box = self.tabs[tab_name]['entrybox']
-        message = self.get_text(entry_box)
+
+        message =self.get_text(entry_box)
         print(message)
         if(message[0]=='/'):
             n = message.find(' ')
             to_send = message[1:n].upper() + message[n:]
             text_box = self.tabs[self.current_tab_name]['textbox']
-            text_box.after(0, self.insert_text(text_box, message[n + 1:]))
+
             if (message[0:5].upper() == '/JOIN'):
+                if(message[6] != '#'):
+                    messagebox.showinfo(message='Nazwa kanału musi zaczynać się od #.', icon='warning')
+                    return
+
                 self.tabs[self.current_tab_name]['tab'].destroy()
                 self.create_channel_tab(message[message.find(' ')+1:])
-                #self.tabs[self.current_tab_name]['tab'] = message[message.find(' ')+1:]
-                self.current_tab_name  = message[message.find(' ')+1:]
+                self.current_tab_name = message[message.find(' ')+1:]
+                text_box = self.tabs[self.current_tab_name]['textbox']
 
+            if(message[0:5].upper()=='/NICK'):
+                m = (message[7:]).find(' ')
+                if (m != -1):
+                    messagebox.showinfo(message='Nick nie może zawierać spacji.', icon='warning')
+                    return
+                else:
+                    self.nick = message[6:]
+            if (message[0:5].upper() == '/HELP'):
+                help_text = "Dostępne komendy:\n" \
+                            "/nick nowy_nick\n" \
+                            "~nick nie powinien zawierać spacji\n" \
+                            "/join #nazwa_kanału\n" \
+                            "~nazwa kanału powinna zaczynać się od #\n" \
+                            "/privmsg nazwa_użytkownika wiadomośc\n"
+                text_box.after(0, self.insert_text(text_box, help_text))
+                return
+
+
+            text_box.after(0, self.insert_text(text_box, message[n + 1:]))
 
         else:
             to_send = 'PRIVMSG ' + self.current_tab_name + ' :' + message
             text_box = self.tabs[self.current_tab_name]['textbox']
-            text_box.after(0, self.insert_text(text_box, self.nick + ': '+message))
-
+            now = datetime.now()
+            time = now.strftime("<%H:%M> ")
+            text_box.after(0, self.insert_text(text_box, time + self.nick + ': '+message))
+        #Wysyłanie wiadomości do serwera
         self.client.send(to_send + '\n')
 
 
-    def join_channel(self, number):
-        print('channel: '+ str(number))
-        self.client.join_channel(number)
 
+    #Parsowanie odpowiedzi od serwera
     def parse_response(self, response):
-        print(response)
         ans = ''
         n = response.find(' ')
         substr = response[0:n]
@@ -194,25 +217,26 @@ class Window(Frame):
         else:
             n = response.find(':', 1)
             ans = response[n+1:]
-        return ans
+        now = datetime.now()
+        time = now.strftime("<%H:%M> ")
 
+        return time + ans
 
+    #Wątek odbierający wiadomości
     def read(self):
         while 1:
             response = self.client.get_response()
             response = self.parse_response(response)
-
             text_box = self.tabs[self.current_tab_name]['textbox']
-            print(response)
             text_box.after(0, self.insert_text(text_box, response))
 
+    #Dodawanie tekstu do okienka czatu
     def insert_text(self, frame, text):
         frame.configure(state='normal')
         frame.insert(END, text + '\n')
-        # Autoscroll to the bottom
         frame.yview(END)
 
-
+#Start programu
 if __name__ == '__main__':
     root = Tk()
     app = Window(root)
